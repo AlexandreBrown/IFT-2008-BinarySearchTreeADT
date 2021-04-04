007df78282beec07fcf8f1158236aa15329e3b5f
Alexandre Brown <alexandre.brown@microdrones.com>
2021-04-04 13:24:49 -0400

    Modified gitignore

--- BinaryTrees/cmake-build-debug-coverage/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brownthedev/Documents/uLaval/Hiver2021/IFT-2008 Algorithmes et structures de données/IFT-2008-BinarySearchTreeADT/BinaryTrees
-# Build directory: /home/brownthedev/Documents/uLaval/Hiver2021/IFT-2008 Algorithmes et structures de données/IFT-2008-BinarySearchTreeADT/BinaryTrees/cmake-build-debug-coverage
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/test")
